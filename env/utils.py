801922bdd66e834b5302e9a3b4a38d7655ff77b6
Sureshpro171005 <sureshpro17@gmail.com>
2026-04-07 00:14:41 +0530

    Meta Prototype

--- env/utils.py
+++ env/utils.py
@@ -1,10 +1,9 @@
-from sentence_transformers import SentenceTransformer, util
-
-model = SentenceTransformer("all-MiniLM-L6-v2")
-
 def calculate_similarity(resume, job):
-    emb1 = model.encode(resume, convert_to_tensor=True)
-    emb2 = model.encode(job, convert_to_tensor=True)
+    resume_words = set(resume.lower().split())
+    job_words = set(job.lower().split())
 
-    score = util.cos_sim(emb1, emb2).item()
-    return float(max(0, min(score, 1)))
+    if len(job_words) == 0:
+        return 0.0
+
+    score = len(resume_words & job_words) / len(job_words)
+    return score
