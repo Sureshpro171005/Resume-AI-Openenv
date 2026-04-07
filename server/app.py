a466160571af810a2aaf6f7818b2d2d3ab126ecf
Sureshpro171005 <sureshpro17@gmail.com>
2026-04-07 23:15:05 +0530

    rewrite server/app.py with double quotes

--- server/app.py
+++ server/app.py
@@ -3,16 +3,20 @@ import uvicorn
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"status": "ok"}
+
 
 @app.post("/reset")
 def reset():
     return {"status": "reset ok"}
 
+
 def main():
     uvicorn.run(app, host="0.0.0.0", port=7860)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
